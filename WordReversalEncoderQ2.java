12be328addd8ac611d6d212a23a5940cc3d0175d
Bunny719939 <gamitirth1@gmail.com>
2026-09-12 02:51:51 +0530

    Implement word reversal encoder in WordReversalEncoderQ2

---
+++ WordReversalEncoderQ2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class WordReversalEncoderQ2 {
+
+    static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder reversedWord = new StringBuilder(words[i]);
+            reversedWord.reverse();
+
+            result.append(reversedWord);
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String sentence = scanner.nextLine();
+
+        String result = reverseEachWord(sentence);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
