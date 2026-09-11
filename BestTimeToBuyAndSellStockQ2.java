323b8b4975d7fdd4eabb95279f0ae1e89eb264a3
Bunny719939 <gamitirth1@gmail.com>
2026-09-12 03:03:04 +0530

    Add BestTimeToBuyAndSellStock
    Implement algorithm to calculate maximum profit from stock prices.

---
+++ BestTimeToBuyAndSellStockQ2.java
@@ -0,0 +1,24 @@
+public class BestTimeToBuyAndSellStockQ2 {
+    static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int lowestPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < lowestPrice) {
+                lowestPrice = prices[i];
+            }
+            int profit = prices[i] - lowestPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+}
