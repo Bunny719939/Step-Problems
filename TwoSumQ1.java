895153060810f6635cc8a5014b0fd46576110514
Bunny719939 <gamitirth1@gmail.com>
2026-09-12 03:01:44 +0530

    Add TwoSum
    Implement a method to find indices of two numbers that sum to a target.

---
+++ TwoSumQ1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoSumQ1 {
+
+    static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println(Arrays.toString(result));
+
+        scanner.close();
+    }
+}
